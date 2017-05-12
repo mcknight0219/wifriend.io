ba2ad29fb3078006bfb488fe6cfe3039a456efb6
Qiang Guo <mcknight0219@gmail.com>
2017-05-11 22:16:27 -0600

    styling editor

--- app/javascript/packs/components/editor.vue
+++ app/javascript/packs/components/editor.vue
@@ -3,11 +3,17 @@
         <div class="fluid-container">
             <div class="columns">
                 <div class="column is-6 is-offset-3">
+                     <div class="field">
+                        <label class="label">Title</label>
+                        <p class="control">
+                            <input class="input" type="text" placeholder="牛逼的名字">
+                        </p>
+                    </div>
                     <nav class="level is-mobile">
                         <div class="level-left">
                             <div class="level-item">
                                 <a class="button is-white is-focused">
-                                    <i class="fa fa-indent" aria-hidden="true"></i>
+                                    <i class="fa fa-eye" aria-hidden="true"></i>
                                 </a>
                                 <a class="button is-white">
                                     <i class="fa fa-bold" aria-hidden="true"></i>
@@ -29,17 +35,9 @@
                                 </a>
                             </div>
                         </div>
-                        <div class="level-right">
-                            <div class="level-item">
-                                <a class="button is-light">
-                                    <i class="fa fa-eye" aria-hidden="true"></i>
-                                </a>
-                            </div>
-                        </div>
                     </nav>
                     <div class="editing-area">
-                        <textarea placeholder="开始写作吧">
-    
+                        <textarea placeholder="开始创作吧" v-on:dblclick="toggleMode">
                         </textarea>
                         <div class="field is-grouped">
                             <p class="control">
@@ -60,6 +58,18 @@
 
 export default {
     props: {
+    },
+
+    data () {
+        return {
+
+        }
+    },
+
+    methods: {
+        toggleMode () {
+            console.log("Hello World")
+        }
     }
 }
 </script>
@@ -71,7 +81,7 @@ export default {
         height: 450px;
         resize: none;
         font-size: 20px;
-        padding: 15px 12px 0 12px;
+        padding: 0px 12px 0 12px;
         border: none;
         outline: none;
         box-shadow: none;
