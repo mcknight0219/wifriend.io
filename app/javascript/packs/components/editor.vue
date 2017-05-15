804935a0c2ef87d67c8f3325143632ba85f4381f
Qiang Guo <qiang.guo@gettyimages.com>
2017-05-15 14:27:54 -0600

    Use CodeMirror as editor

--- app/javascript/packs/components/editor.vue
+++ app/javascript/packs/components/editor.vue
@@ -3,10 +3,19 @@
         <div class="fluid-container">
             <div class="columns">
                 <div class="column is-6 is-offset-3">
-                     <div class="field">
+                    <article class="message is-danger" v-if="hasError">
+                        <div class="message-header">
+                            <p>Warning</p>
+                            <button class="delete" @click="closeNotice"></button>
+                        </div>
+                        <div class="message-body">
+                            题目或文章内容不能为空    
+                        </div>
+                    </article>
+                    <div class="field">
                         <label class="label">Title</label>
                         <p class="control">
-                            <input class="input" type="text" placeholder="牛逼的名字">
+                            <input class="input" type="text" placeholder="牛逼的名字" v-model="title">
                         </p>
                     </div>
                     <nav class="level is-mobile">
@@ -35,18 +44,22 @@
                                 </a>
                             </div>
                         </div>
-                    </nav>
-                    <div class="editing-area">
-                        <textarea placeholder="开始创作吧" v-on:dblclick="toggleMode">
-                        </textarea>
-                        <div class="field is-grouped">
-                            <p class="control">
-                                <button class="button is-primary">Submit</button>
-                            </p>
-                            <p class="control">
-                                <button class="button is-link">Cancel</button>
-                            </p>
+                        <div class="level-right">
+                            <div class="level-item">
+                                <a class="button is-white" @click="toggleFullscreen">
+                                    <i class="fa fa-arrows-alt" aria-hidden="true"></i>
+                                </a>
+                            </div>
                         </div>
+                    </nav>
+                    <div id="editor"></div>
+                    <div class="field is-grouped">
+                        <p class="control">
+                            <button class="button is-primary" @click="publish">发布</button>
+                        </p>
+                        <p class="control">
+                            <button class="button is-link">取消</button>
+                        </p>
                     </div>
                 </div>
             </div>
@@ -55,41 +68,82 @@
 </template>
 
 <script>
+import CodeMirror from 'codemirror'
+import 'codemirror/mode/markdown/markdown'
 
 export default {
     props: {
     },
 
-    data () {
+    data() {
         return {
-
+            title: '',
+            hasError: false
         }
     },
 
     methods: {
-        toggleMode () {
-            console.log("Hello World")
+        publish () {
+            const text = this.editor.getValue()
+            const title = this.title
+
+            if (title.length === 0 || text.length === 0) {
+                this.hasError = true
+                return
+            }
+        },
+
+        closeNotice () {
+            this.hasError = false
+        },
+
+        toggleFullscreen() {
+            debugger
+            const el = document.getElementById("editor")
+            const isFull = document.isFull || document.mozFullScreen || document.webkitIsFullScreen
+
+            const requestFull = () => {
+                if (el.requestFullScreen) {
+                    el.requestFullScreen()
+                } else if (el.mozRequestFullScreen) {
+                    el.mozRequestFullScreen()
+                } else if (el.webkitRequestFullScreen) {
+                    el.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT)
+                }
+            }
+
+            const cancel = () => {
+                if (document.cancelFullScreen) {
+                    document.cancelFullScreen()
+                } else if (document.mozCancelFullScreen) {
+                    document.mozCancelFullScreen()
+                } else if (document.webkitCancelFullScreen) {
+                    document.webkitCancelFullScreen()
+                }
+            }
+
+            isFull ? cancel() : requestFull()
         }
+    },
+
+    mounted() {
+        this.editor = CodeMirror(document.getElementById("editor"), {
+            mode: 'markdown',
+            theme: 'paper',
+            tabSize: '2',
+            indentWithTabs: true,
+            lineNumbers: false,
+            autofocus: true,
+            extraKeys: {
+                'Enter': '',
+                'Tab': '',
+                'Shift-Tab': ''
+            }
+        })
     }
 }
 </script>
 
 <style lang="scss">
-.editing-area {
-    textarea {
-        width: 100%;
-        height: 450px;
-        resize: none;
-        font-size: 20px;
-        padding: 0px 12px 0 12px;
-        border: none;
-        outline: none;
-        box-shadow: none;
-        caret-color: aqua;
-    }
-}
 
-.level {
-    bottom: 0px;
-}
 </style>
