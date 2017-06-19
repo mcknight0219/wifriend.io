4f2b108f2f731aabb72a5a322e3959cebf2fcbea
Qiang Guo <mcknight0219@gmail.com>
2017-06-19 11:11:16 -0600

    Never worked

--- config/webpack/loaders/sass.js
+++ config/webpack/loaders/sass.js
@@ -9,9 +9,7 @@ module.exports = {
       { 
         loader: 'css-loader', 
         options: { 
-          minimize: env.NODE_ENV === 'production',
-          modules: true,
-          localIdentName: '[path][name]__[local]--[hash:base64:5]' 
+          minimize: env.NODE_ENV === 'production'
         } 
       },
       'postcss-loader',
